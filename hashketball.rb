d0ddebd03cfedb1d984ddcefa9bea6f1b5d91960
Sid Jagarlamudi <jaglings@gmail.com>
2020-04-29 18:55:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,17 +177,4 @@ end
 jersey_numbers
 end
 
-def player_stats(name)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |player|
-          if name == player[:player_name]
-            player.delete(:player_name) 
-            return player
-          end
-        end
-      end
-    end
-  end
-end
+
